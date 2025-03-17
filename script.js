3169e060b34dcf2c0509eacbbc6779b5de079383
Dương Unity (Jake) <jakediep1902@gmail.com>
2025-03-17 23:51:47 +0700

    Update

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ function sayHello(name) {
     console.log("Hello, ".concat(name, "!"));
 }
 sayHello("TypeScript");
+function GetSCkey()
+{
+    return "IPR43Z1OX7DYWORMIXIRZNFYDZ4GTOYNOX75N1N8UDDSCFY3WT"
+}
+function GetTitleID()
+{
+    return "CEB9E"
+}
+// Đảm bảo code chạy sau khi DOM đã tải
+document.addEventListener("DOMContentLoaded", function () {
+    const htmlContent = `
+        <h1>Trà sữa XUCANAXIMUI xin chào</h1>
+        <p>Uy tín, Chất lượng, Tận tâm</p>
+    `;
+
+    document.getElementById("content").innerHTML = htmlContent;
+});
